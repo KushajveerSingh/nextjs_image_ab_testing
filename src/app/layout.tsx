774cb3542dba5b19d575e2c91af6507bb5589656
Kushajveer Singh <kushajreal@gmail.com>
2024-02-20 15:59:15 -0500

    add beam analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
+
 import './globals.css';
 
 import { Providers } from '@/providers/providers';
@@ -21,6 +23,13 @@ export default function RootLayout({
       lang='en'
       suppressHydrationWarning
     >
+      <head>
+        <Script
+          src='https://beamanalytics.b-cdn.net/beam.min.js'
+          data-token='7b93b436-c2e2-4be3-b042-8e41b6afdaf6'
+          async
+        ></Script>
+      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
